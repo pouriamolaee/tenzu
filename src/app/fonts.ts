6f6be3df91efd5c3469a612c7d02cf1b00f813fc
pouria <pouria@uppfirst.com>
2024-07-18 12:28:53 +0330

    chore: Add fonts to project

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Merriweather } from "next/font/google";
+
+export const serif = Merriweather({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+});
