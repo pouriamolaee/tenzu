af04e8927b1af370ea679cf57556f3e1f152a0e4
pouria <pouria@uppfirst.com>
2024-07-18 12:55:47 +0330

    fix: Add the page title font

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,11 @@
-import { Merriweather } from "next/font/google";
+import { Merriweather, Montserrat } from "next/font/google";
+
+export const sans = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700", "900"],
+  style: ["normal"],
+});
 
 export const serif = Merriweather({
   subsets: ["latin"],
